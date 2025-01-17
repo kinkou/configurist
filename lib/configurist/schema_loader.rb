95ae8e872799cf0ffb02a3927fbc62f3c897c50e
Sergey Konotopov <sergey.konotopov@gmail.com>
2025-01-18 02:58:53 +0300

    Add Configurist::SchemaLoader

---
+++ lib/configurist/schema_loader.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Configurist
+  class SchemaLoader
+    InvalidSchemaError = Class.new(StandardError)
+
+    def call
+      Configurist.schemas.clear
+
+      schema_files.each do |schema_file|
+        schema = YAML.load_file(schema_file)
+
+        validate_schema!(schema:)
+
+        schema_id = schema['_id']
+        Configurist.schemas[schema_id] = schema
+      end
+    end
+
+    private
+
+    def schema_files
+      Configurist::SchemaFilesLocator.new.call
+    end
+
+    def validate_schema!(schema:)
+      error_messages = Configurist::Validator.new.validate_schema(schema:)
+      return if error_messages.blank?
+
+      raise(InvalidSchemaError, error_messages.join('; '))
+    end
+  end
+end
---
+++ spec/configurist/schema_loader_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+RSpec.describe Configurist::SchemaLoader do
+  let(:perform) { described_class.new.call }
+
+  it 'loads available schemas to Configurist.schemas', :aggregate_failures do
+    expect(Configurist.schemas).to eq({})
+
+    perform
+
+    expect(Configurist.schemas.keys.size).to eq(1)
+    expect(Configurist.schemas.dig('default', 'description')).to eq('Default scope schema for tests')
+  end
+
+  it 'completely reloads schemas on each call', :aggregate_failures do
+    Configurist.schemas = { 'invalid' => 'data' }
+
+    perform
+
+    expect(Configurist.schemas).not_to eq({ 'invalid' => 'data' })
+  end
+end
