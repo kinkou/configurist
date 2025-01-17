a5f223f98396985de6091760190612b21dd312e6
Sergey Konotopov <sergey.konotopov@gmail.com>
2025-01-18 02:29:57 +0300

    Add Configurist::SchemaFilesLocator

---
+++ lib/configurist/schema_files_locator.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Configurist
+  class SchemaFilesLocator
+    SchemaFileError = Class.new(StandardError)
+
+    SCHEMAS_LOCATION = 'config/configurist_schemas/**/*.yml'
+
+    def call
+      files = find_schema_files
+
+      raise(SchemaFileError, 'No schema files found') if files.empty?
+
+      files.filter(&:file?)
+    end
+
+    private
+
+    def find_schema_files
+      return Rails.root.glob(SCHEMAS_LOCATION) if defined?(Rails)
+
+      Pathname.new(__dir__).join('../../').glob(SCHEMAS_LOCATION) # For hand-testing locally without Rails
+    end
+  end
+end
---
+++ spec/configurist/schema_files_locator_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+RSpec.describe Configurist::SchemaFilesLocator do
+  let(:perform) { described_class.new.call }
+
+  context 'when Rails is defined' do
+    let(:rails) { class_double('Rails', root: gem_schema_files_path) } # rubocop:disable RSpec/VerifiedDoubleReference
+
+    before do
+      stub_const('Rails', rails)
+    end
+
+    context 'when schema files are found' do
+      let(:gem_schema_files_path) { Pathname.new(__dir__).join('../../') }
+
+      it 'calls Rails.root' do
+        perform
+
+        expect(rails).to have_received(:root)
+      end
+
+      it 'returns an array of Pathname objects pointing to schema files', :aggregate_failures do
+        expect(perform).to be_an(Array)
+        expect(perform.first).to be_a(Pathname)
+        expect(perform.first.extname).to eq('.yml')
+      end
+
+      it 'filters out directories with .yml extension', :aggregate_failures do
+        expect(perform.size).to eq(1)
+        expect(perform.first.basename.to_s).to eq('default.yml')
+      end
+    end
+
+    context 'when no schema files are found' do
+      let(:gem_schema_files_path) { Pathname.new(__dir__) }
+
+      it 'raises an exception' do
+        expect { perform }.to raise_error(described_class::SchemaFileError)
+      end
+    end
+  end
+
+  context 'when Rails is not defined' do
+    let(:gem_schema) { YAML.load_file(perform.first) }
+
+    it 'uses test schema files from the gem' do
+      expect(gem_schema['description']).to eq('Default scope schema for tests')
+    end
+  end
+end
