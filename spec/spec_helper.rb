0df7f3599cb92115130f7299b71aae63ca9292a7
Sergey Konotopov <sergey.konotopov@gmail.com>
2025-01-20 07:04:47 +0300

    Add shoulda-matchers gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,3 +14,14 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 end
+
+require 'shoulda-matchers'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    with.library :active_record
+    with.library :active_model
+  end
+end
